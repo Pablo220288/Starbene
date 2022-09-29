b6433ff10ce6f1ab90b5e70f66d5727ed891f61e
Pab Her <peh_tj@hotmail.com>
2022-09-29 11:26:39 -0300

    Seccion Productos

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,4 @@
+//Nav Hamburger
 const hamburger = document.querySelector('.hamburger');
 const nav_mobile = document.querySelector('.nav-mobile-container');
 
@@ -5,3 +6,9 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('is-active');
     nav_mobile.classList.toggle('is-active');
 });
+
+//Nav Scroll
+window.addEventListener('scroll',() => {
+    let nav = document.querySelector('.nav');
+    nav.classList.toggle('nav-scroll',window.scrollY>10)
+})
